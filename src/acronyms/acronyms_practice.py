bebc48fe3a71593657e70f9bb882128a261fd7af
MicheleMorelli <morelli.michele@hotmail.it>
2019-05-12 17:18:00 +0000

    fixed issue with the python script

--- src/acronyms/acronyms_practice.py
+++ src/acronyms/acronyms_practice.py
@@ -1,16 +1,11 @@
+import os
 from typing import Dict
 import re
 import random
 
-def main()->None:
-    file_name = 'acronyms_list'
-    all_acronyms = get_all_acronyms(file_name)
-    for i in range(10):
-        key = random.choice(list(all_acronyms))
-        print("{}:".format(key), end='')
-        process_answer(all_acronyms[key], input())
-    print("PRACTICE SESSION OVER!")
-
+ROUNDS = 10
+PATH = os.path.dirname(os.path.realpath(__file__))
+DATA = "{}/acronyms_list".format(PATH)
 
 def get_all_acronyms(file_name:str)->Dict[str,str]:
     all_acronyms={}
@@ -29,6 +24,26 @@ def process_answer(answer:str, inp:str)->None:
         print("-------> WRONG! CORRECT ANNSWER: {}".format(answer))
 
     print("{}\n".format("="*20))
+
+
+def print_title()->None:
+    print("{}\n".format("+"*20))
+    print("IWT ACRONYMS PRACTICE\n")
+    print("{}\n".format("+"*20))
+
+
+def run(all_acronyms:Dict[str,str])->None:
+    print_title()
+    for i in range(ROUNDS):
+        key = random.choice(list(all_acronyms))
+        print("{}:".format(key), end='')
+        process_answer(all_acronyms[key], input())
+    print("\nPRACTICE SESSION OVER!\n")
+
+
+def main()->None:
+    all_acronyms = get_all_acronyms(DATA)
+    run(all_acronyms)
 
 
 if __name__ == '__main__':
