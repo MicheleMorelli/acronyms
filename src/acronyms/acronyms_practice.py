1ffea689d9c80c297d63e119cefec812fb3b7abe
MicheleMorelli <morelli.michele@hotmail.it>
2019-05-13 00:25:40 +0000

    refactor + linted

--- src/acronyms/acronyms_practice.py
+++ src/acronyms/acronyms_practice.py
@@ -7,32 +7,38 @@ ROUNDS = 10
 PATH = os.path.dirname(os.path.realpath(__file__))
 DATA = "{}/acronyms_list".format(PATH)
 
-def get_all_acronyms(file_name:str)->Dict[str,str]:
-    all_acronyms={}
+
+def get_all_acronyms(file_name: str)->Dict[str, str]:
+    lines = []
     with open(file_name, 'r') as fh:
-        for line in fh.readlines():
-            if (re.search('^.*?=.*?.$',line)):
-                acronym, meaning = re.split('=',line)
-                all_acronyms[acronym.strip()] = meaning.strip()
-    return all_acronyms
+        lines = fh.readlines()
+    lines = list(filter(lambda x: re.search('^.*?=.*?.$', x), lines))
+    acronyms = [tuple(re.split('=', line)) for line in lines]
+    return {k.strip(): v.strip() for k, v in acronyms}
 
 
-def process_answer(answer:str, inp:str)->None:
-    if (inp.lower() == answer.lower()):
+def process_answer(answer: str, inp: str)->None:
+    #normalising strings to ensure that 'hypertext', 'hyper text' and
+    #'hyper-text' match
+    n = lambda s: re.sub('[ -]', "", s).lower()
+    if n(inp) == n(answer):
         print("-------> CORRECT!")
     else:
         print("-------> WRONG! CORRECT ANNSWER: {}".format(answer))
+    print_line("=")
 
-    print("{}\n".format("="*20))
+
+def print_line(c: str)->None:
+    print("{}\n".format(c*20))
 
 
 def print_title()->None:
-    print("{}\n".format("+"*20))
+    print_line("/")
     print("IWT ACRONYMS PRACTICE\n")
-    print("{}\n".format("+"*20))
+    print_line("/")
 
 
-def run(all_acronyms:Dict[str,str])->None:
+def run(all_acronyms: Dict[str, str])->None:
     print_title()
     for i in range(ROUNDS):
         key = random.choice(list(all_acronyms))
