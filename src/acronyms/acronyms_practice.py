252b5b41171c4fb5799248648a6cb17ebc240f1d
MicheleMorelli <michele.morelli@london.ac.uk>
2019-05-15 09:54:54 +0000

    code linted

--- src/acronyms/acronyms_practice.py
+++ src/acronyms/acronyms_practice.py
@@ -1,3 +1,8 @@
+'''
+Simple program to help remember the networking/TCP/IP/Internet-related acronyms.
+-Michele Morelli, May 2019-
+'''
+
 import os
 from typing import Dict
 import re
@@ -10,9 +15,8 @@ DATA = "{}/acronyms_list".format(PATH)
 
 def get_all_acronyms(file_name: str)->Dict[str, str]:
     lines = []
-    with open(file_name, 'r') as fh:
-        lines = fh.readlines()
-    lines = list(filter(lambda x: re.search('^.*?=.*?.$', x), lines))
+    with open(file_name, 'r') as f_h:
+        lines = list(filter(lambda x: re.search('^.*?=.*?.$', x), f_h.readlines()))
     acronyms = [tuple(re.split('=', line)) for line in lines]
     return {k.strip(): v.strip() for k, v in acronyms}
 
@@ -20,16 +24,16 @@ def get_all_acronyms(file_name: str)->Dict[str, str]:
 def process_answer(answer: str, inp: str)->None:
     #normalising strings to ensure that 'hypertext', 'hyper text' and
     #'hyper-text' match
-    n = lambda s: re.sub('[ -]', "", s).lower()
-    if n(inp) == n(answer):
+    normalise = lambda string: re.sub('[ -]', "", string).lower()
+    if normalise(inp) == normalise(answer):
         print("-------> CORRECT!")
     else:
         print("-------> WRONG! CORRECT ANNSWER: {}".format(answer))
     print_line("=")
 
 
-def print_line(c: str)->None:
-    print("{}\n".format(c*20))
+def print_line(char: str)->None:
+    print("{}\n".format(char*20))
 
 
 def print_title()->None:
